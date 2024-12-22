cc7aa8f52e4c90415fa1e0c1e737d02564ce7097
Hariprasath M <hariprasathm777@gmail.com>
2024-12-22 19:08:13 +0530

    frontend added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/products.route.js';
 
@@ -8,6 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5070;
 
+const __dirname = path.resolve();
 app.use(express.json());
 
 app.use('/api/products',productRoutes);
